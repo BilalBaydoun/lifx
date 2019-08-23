5b1f88d8d4f53bf6a6a5d4a1a4e3dacbfd7397a1
BilalBaydoun <bilal.baydoun@me.com>
2019-08-23 18:17:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = "https://api.lifx.com/v1/lights/all";
+$link = "https://api.lifx.com/v1/lights/Tv";
 $authToken = "cb23f82e367123e90803d1b12f0b0b5978a18f4c9f403c69e5f1222b9a0b1bb5";
 $ch = curl_init($link);
 $headers = array('Authorization: Bearer ' . $authToken);
